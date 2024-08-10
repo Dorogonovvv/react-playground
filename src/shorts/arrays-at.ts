37afdc0335cfeb9c17369cb6ed2148cb76bac212
Dima Dorogonov <dmitriy@sprinque.com>
2024-08-10 23:01:01 +0100

    learn: arrays-at

---
+++ src/shorts/arrays-at.ts
@@ -0,0 +1,9 @@
+// https://www.youtube.com/@IntegratorDima
+// Array.prototype.at()
+// ECMAScript 2022
+
+const fruits = ['apple', 'banana', 'cherry'];
+
+console.log(fruits.at(1));
+console.log(fruits.at(-1));
+console.log(fruits[fruits.length - 1])
